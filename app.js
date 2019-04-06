2d8fb7d092bddb57c4f20d0b0d85e7375f7a2bd0
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-07 01:49:25 +0530

    geocoding using the google geocode API

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var request = require('request');
 
-var urlString='https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCGuNZA83IRGZxd8LVqhCT5aYv9oc8cIC0&address=1301%20lombard%20street%20philadelphia'
+var urlString='https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBXksvM0qgO-vKhQ-Z9siG5dAIdav7hI5U&address=1301%20lombard%20street%20philadelphia'
 request({
     url :urlString,
     json:true
 },(err,res,body)=> {
-    console.log(body)
+    // console.log(JSON.stringify(body,undefined,2))
+    // console.log(res)
+    console.log(`Address : ${body.results[0].formatted_address},\nLatitude:${body.results[0].geometry.location.lat}\nLongitude:${body.results[0].geometry.location.lng}`);
 });
