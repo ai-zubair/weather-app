0126aa4285a3a8c287e694c037f243125515b664
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-07 02:54:08 +0530

    add support for dynamic address

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-var request = require('request');
+const request = require('request');
+const yargs = require('yargs');
 
-var urlString='https://maps.googleapis.com/maps/api/geocode/json?key=[YOUR_API_KEY]&address=1301%20lombard%20street%20philadelphia'
+const argv = yargs.options({
+        a : {
+            describe : 'Address of the location to fetch weather for.',
+            demand : 'true',
+            alias : 'address',
+            string : true
+        }
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+
+const address = argv.address;
+
+if(address.length === 0) {
+    console.log('Please provide a valid address!')
+    return;
+}
+console.log(address)
+console.log(argv)
+var encodedAddress = encodeURIComponent(address)
+
+var urlString=`https://maps.googleapis.com/maps/api/geocode/json?key=[YOU_API_KEY]&address=${encodedAddress}`
+
+
 request({
     url :urlString,
     json:true
