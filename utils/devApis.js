289923153dc5adf290ac08ea1838080ff70a80bc
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-07 17:01:53 +0530

    Delete devApis.js

--- utils/devApis.js
+++
@@ -1,8 +0,0 @@
-const devAPIs = {
-    geocodeAPI : {
-        url : 'https://maps.googleapis.com/maps/api/geocode/json',
-        key : 'AIzaSyBSnwJUoxme8gcSu5IV0U9T9_y4o0EaBzo' 
-    }
-}
-
-module.exports = devAPIs;
